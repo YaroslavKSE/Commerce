3af7f8255e792e799bd3bd46c8974b998ff4a755
YaroslavKSE <ikhomych@kse.org.ua>
2023-03-09 15:47:16 +0200

    Added "create_listing" function in views.py, which creates a new listing. Added "create_listing.html" to provide user interface.

---
+++ commerce/auctions/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=2000)
+    bid = models.IntegerField()
+
+
+def __str__(self):
+    return f"Title: {self.title} Bid: {self.bid}" \
+           f"Description: {self.description}"
